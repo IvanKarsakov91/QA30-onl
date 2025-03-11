2151f595d337f13027041bacc073f08681ef1334
IvanKarsakov <IvanKarsakov91@gmail.com>
2025-03-11 23:41:08 +0300

    Комментарий

--- src/homework_3/HomeWork.java
+++ src/homework_3/HomeWork.java
@@ -18,3 +18,26 @@ public class HomeWork {
         return new Scanner(System.in);
     }
 }
+
+public class BMI {
+    public static void main(String[] args){
+        startBMI();
+    }
+
+    public static void startBMI(){
+        getInitialMsg();
+        getBMIData();
+    }
+
+    public static void getInitialMsg(){
+        System.out.println("Инструкция калькулятора индекса массы тела");
+        System.out.println("Значения ниже 18.5-Недобор веса");
+        System.out.println("Значения от 18.5 до 24.9-Нормальный вес");
+        System.out.println("Значения от 25 до 29.9-Лишний вес");
+        System.out.println("Значения от 30 до 39.9-Ожирение 1 степени");
+        System.out.println("Значения от 40 Ожирение 2 степени");
+    }
+    public static void getBMIData(){
+
+    }
+}
