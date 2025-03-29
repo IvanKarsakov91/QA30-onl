5b01019128602d192daeda180dd8b54efa0be228
IvanKarsakov <IvanKarsakov91@gmail.com>
2025-03-29 10:08:02 +0300

    Дз7

--- src/homework_7/Task7.java
+++ src/homework_7/Task7.java
@@ -3,9 +3,9 @@ package homework_7;
 public class Task7 {
     public static void main(String[] args) {
         // Создание трех объектов Phone
-        Phone phone1 = new Phone("123456789", "ModelX", 200.5);
-        Phone phone2 = new Phone("987654321", "ModelY", 180.0);
-        Phone phone3 = new Phone("555555555", "ModelZ", 150.0);
+        Phone phone1 = new Phone("554456582", "Iphone", 220.0);
+        Phone phone2 = new Phone("789545632", "Samsung", 250.0);
+        Phone phone3 = new Phone("458565425", "Pocco", 180.0);
 
         // Вывод значений переменных объектов
         phone1.displayInfo();
@@ -13,9 +13,9 @@ public class Task7 {
         phone3.displayInfo();
 
         // Вызов методов
-        phone1.receiveCall("Алиса");
-        phone2.receiveCall("Боб", "123123123");
-        phone3.sendMessage("111111111", "222222222", "333333333");
+        phone1.receiveCall("Зоя");
+        phone2.receiveCall("Вера", "343223453");
+        phone3.sendMessage("4678764532", "5748376578", "645890478");
 
         // Получение номера телефона
         System.out.println("Номер телефона 1: " + phone1.getNumber());
